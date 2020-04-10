123c7ba023b99a550b16ff498d25b643c852975b
root <root@yq01-gpu-88-152-14-00.epc.baidu.com>
2020-04-10 10:11:51 +0000

    test

--- test.cpp
+++ test.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include "test1/test1.h"
+#include <dlfcn.h>
+
 using namespace std;
+
+int func()
+{
+    static int a = 1;
+    a++;
+    return a;
+}
 
 int main(int argc, char *argv[])
 {
-    std::cout << "你好，世界" << std::endl;
+    GetModuleFileName();
+    std::cout << func() << std::endl;
+    Dl_info info;
+    int rc = dladdr((void*)fun1, &info);
+    fun1();
+    std::cout << info.dli_fname << std::endl;
 }
